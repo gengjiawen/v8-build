cc5b6e80223e31c993e2e12714d20d031d531fe4
gengjiawen <technicalcute@gmail.com>
2019-03-10 17:52:09 +0800

    split android build from android
    Signed-off-by: gengjiawen <technicalcute@gmail.com>

--- publish_docker.js
+++ publish_docker.js
@@ -3,7 +3,7 @@ const execSync = require("child_process").execSync;
 function publishDocker() {
   const now = new Date()
   const repo = `gengjiawen/v8-build`
-  const finaltag = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
+  const finaltag = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
   const cmd =`
 docker login -u ${process.env.DOCKER_USER} -p ${process.env.DOCKER_PASS}
 docker build -t ${repo}:${finaltag} .
